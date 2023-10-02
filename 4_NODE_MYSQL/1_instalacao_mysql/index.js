a81e657722fd7ccebf4bb775274bf055b87547ef
Felos <felmsilva@gmail.com>
2023-10-02 19:35:24 -0300

    pronto

--- 4_NODE_MYSQL/1_instalacao_mysql/index.js
+++ 4_NODE_MYSQL/1_instalacao_mysql/index.js
@@ -4,10 +4,28 @@ const mysql = require("mysql")
 
 const app = express()
 
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+    res.render('home')
+} )
 const conn = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
     database: "nodemysql",
     port: 3307
+})
+
+conn.connect((error) => {
+    if(error){
+        console.log(error)
+        return
+    }
+    console.log('conectou ao mysql')
+
+    app.listen(3000, () => {
+        console.log('servidor rodando na porta 3000')
+    })
 })
